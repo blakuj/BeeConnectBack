118669bd7ce4a85812b2827c398f4e9c957efdd3
fifii123 <filip.rozmyslowski@op.pl>
2025-10-10 13:40:46 +0200

    Test commit

--- src/main/java/com/beconnect/beeconnect_backend/Controller/PersonController.java
+++ src/main/java/com/beconnect/beeconnect_backend/Controller/PersonController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-
+//
 @RestController
 @RequestMapping("/api/profile")
 public class PersonController {
