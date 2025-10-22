8a067ad6715715d606232fccf4a3b4d10d413a99
krzys <luk.pietka@gmail.com>
2025-10-22 19:43:54 +0200

    Changed AreaDTO

--- src/main/java/com/beconnect/beeconnect_backend/DTO/AreaDTO.java
+++ src/main/java/com/beconnect/beeconnect_backend/DTO/AreaDTO.java
@@ -7,15 +7,22 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class AreaDTO {
+    private Long id;
     private String type;
     private List<List<Double>> coordinates;
     private double area;
     private String description;
     private int maxHives;
     private double pricePerDay;
-    private String status;
+    private AvailabilityStatus status;
     private String ownerFirstName;
     private String ownerLastName;
 
+    private LocalDate availableFrom ;
+    private String imgBase64;
+
+    private String name;
 }
