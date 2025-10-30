f89abc6d704c8503e25a94c6a96c4a2a135da18e
Łukasz <s24509@pjwstk.edu.pl>
2025-10-30 12:26:18 +0100

    Add essential imports and findByTenant and findByTenantAndStatus methods to ReservationRepository

--- src/main/java/com/beconnect/beeconnect_backend/Repository/ReservationRepository.java
+++ src/main/java/com/beconnect/beeconnect_backend/Repository/ReservationRepository.java
@@ -1,7 +1,28 @@
 package com.beconnect.beeconnect_backend.Repository;
+import com.beconnect.beeconnect_backend.Enum.ReservationStatus;
+import com.beconnect.beeconnect_backend.Model.Area;
+import com.beconnect.beeconnect_backend.Model.Person;
 import com.beconnect.beeconnect_backend.Model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import com.beconnect.beeconnect_backend.Enum.ReservationStatus;
+import com.beconnect.beeconnect_backend.Model.Area;
+import com.beconnect.beeconnect_backend.Model.Person;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.time.LocalDate;
+import java.util.List;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+
+
+    List<Reservation> findByTenant(Person tenant);
+
+    List<Reservation> findByTenantAndStatus(Person tenant, ReservationStatus status);
+
 
 }
