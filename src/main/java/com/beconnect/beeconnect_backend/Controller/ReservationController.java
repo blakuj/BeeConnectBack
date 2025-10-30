2c812c7e0055a6f921b1f50e61b3a5dc6284d86b
Łukasz <s24509@pjwstk.edu.pl>
2025-10-30 12:06:55 +0100

    Create ReservationController and createReservation method

---
+++ src/main/java/com/beconnect/beeconnect_backend/Controller/ReservationController.java
@@ -0,0 +1,31 @@
+package com.beconnect.beeconnect_backend.Controller;
+
+import com.beconnect.beeconnect_backend.DTO.CreateReservationDTO;
+import com.beconnect.beeconnect_backend.DTO.ReservationResponseDTO;
+import com.beconnect.beeconnect_backend.Enum.ReservationStatus;
+import com.beconnect.beeconnect_backend.Service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/reservations")
+public class ReservationController {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @PostMapping
+    public ResponseEntity<?> createReservation(@RequestBody CreateReservationDTO dto) {
+        try {
+            ReservationResponseDTO reservation = reservationService.createReservation(dto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(reservation);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
+}
