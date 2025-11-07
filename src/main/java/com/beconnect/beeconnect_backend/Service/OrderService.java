ace3ff72cb1dd8794f47eae0ebba484beaeb2c8e
Łukasz <s24509@pjwstk.edu.pl>
2025-11-07 20:53:02 +0100

    Add mapToDTO method to OrderService

--- src/main/java/com/beconnect/beeconnect_backend/Service/OrderService.java
+++ src/main/java/com/beconnect/beeconnect_backend/Service/OrderService.java
@@ -30,4 +30,30 @@ public class OrderService {
 
     @Autowired
     private PersonService personService;
+
+    private OrderDTO mapToDTO(Order order) {
+        return OrderDTO.builder()
+                .id(order.getId())
+                .buyerId(order.getBuyer().getId())
+                .buyerFirstname(order.getBuyer().getFirstname())
+                .buyerLastname(order.getBuyer().getLastname())
+                .buyerEmail(order.getBuyer().getEmail())
+                .sellerId(order.getSeller().getId())
+                .sellerFirstname(order.getSeller().getFirstname())
+                .sellerLastname(order.getSeller().getLastname())
+                .sellerEmail(order.getSeller().getEmail())
+                .productId(order.getProduct().getId())
+                .productName(order.getProduct().getName())
+                .productCategory(order.getProduct().getCategory().toString())
+                .productImage(order.getProduct().getImageBase64())
+                .quantity(order.getQuantity())
+                .pricePerUnit(order.getPricePerUnit())
+                .totalPrice(order.getTotalPrice())
+                .status(order.getStatus())
+                .orderedAt(order.getOrderedAt())
+                .deliveredAt(order.getDeliveredAt())
+                .deliveryAddress(order.getDeliveryAddress())
+                .buyerNotes(order.getBuyerNotes())
+                .build();
+    }
 }
