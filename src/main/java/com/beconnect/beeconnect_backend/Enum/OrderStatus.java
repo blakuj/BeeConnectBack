9dbadf459189b2271069756ba5cf274c00cb48c5
Łukasz <s24509@pjwstk.edu.pl>
2025-11-07 20:38:41 +0100

    Create OrderStatus enum

---
+++ src/main/java/com/beconnect/beeconnect_backend/Enum/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.beconnect.beeconnect_backend.Enum;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    COMPLETED,
+    CANCELLED
+}
