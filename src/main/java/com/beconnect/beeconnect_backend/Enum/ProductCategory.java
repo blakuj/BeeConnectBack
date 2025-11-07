bf46c897f0176e4cfd6b45820e439b699c197702
Łukasz <s24509@pjwstk.edu.pl>
2025-11-07 20:38:32 +0100

    Create ProductCategory enum

---
+++ src/main/java/com/beconnect/beeconnect_backend/Enum/ProductCategory.java
@@ -0,0 +1,12 @@
+package com.beconnect.beeconnect_backend.Enum;
+
+public enum ProductCategory {
+    HONEY,
+    WAX,
+    POLLEN,
+    PROPOLIS,
+    ROYAL_JELLY,
+    HONEYCOMB,
+    EQUIPMENT,
+    OTHER
+}
